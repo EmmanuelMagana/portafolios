bace8fdb74869e92f47a44393660ccf296534ab2
EmmanuelMagana <tenzacorp@outlook.com>
2020-11-16 15:14:32 -0600

    Hola mundo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Jemc</title>
+  </head>
+  <body>
+
+
+
+    <?php
+    echo "hola prro";
+     ?>
+  </body>
+</html>
